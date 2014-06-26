8138d08bc256a805329759248bc33f4437f2b6f3
Owais Alam <owaisalam97@gmail.com>
2014-06-27 01:27:18 +0500

    Devise

--- app/controllers/userblogs_controller.rb
+++ app/controllers/userblogs_controller.rb
@@ -18,6 +18,16 @@ class UserblogsController < ApplicationController
   end
 
   def create
+    @user=UserBlog.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Message is successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      
+      else
+        render "new"
+      end
+    end
   end
 
   def index
@@ -25,5 +35,8 @@ class UserblogsController < ApplicationController
   end
   def set_user
     @user=UserBlog.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:message,:user_type,:user_id)
   end
 end
